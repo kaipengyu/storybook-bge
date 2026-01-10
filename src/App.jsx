5ca67b80ffd3124ae53d10d04de6ff2c4f452222
Kaipeng Yu <kaipeng@umich.edu>
2026-01-10 17:54:50 -0500

    Add .gitignore and fix App.jsx import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { Button } from './components/Button'
-
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
-      <h1>Welcome to Storybook</h1>
-      <p>Run <code>npm run storybook</code> to see your components</p>
-      <Button label="Click me" primary />
+      <h1>Email Template Components</h1>
+      <p>Run <code>npm run storybook</code> to see your email components</p>
     </div>
   )
 }
